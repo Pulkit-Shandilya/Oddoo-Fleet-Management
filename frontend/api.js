17ca801a5ff90e1b31745a46ca4eeec87b17c702
CapnBloodBeard <77095391+CapnBloodBeard@users.noreply.github.com>
2026-05-08 04:49:38 +0530

    changes

--- frontend/api.js
+++ frontend/api.js
@@ -6,7 +6,7 @@
 // const API_URL = 'http://localhost:5000/api';
 const API_URL = window.location.hostname === 'localhost' 
   ? 'http://localhost:5000/api' 
-  : '/api';
+  : '/api'; 
 /**
  * Make an authenticated API request
  */
